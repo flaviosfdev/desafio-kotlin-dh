f3f052737e827d34a61f30771dfbcf2235564695
Flávio Ferreira <flaviosf.dev@gmail.com>
2021-09-06 16:12:51 -0300

    Parte C

---
+++ src/main/kotlin/Professor.kt
@@ -0,0 +1,10 @@
+class Professor(
+    val nome: String,
+    val sobrenome: String,
+    var tempoDeCasa: Int,
+    val codigoProfessor: Int
+) {
+
+    override fun equals(other: Any?): Boolean = (other is Professor && this.codigoProfessor == other.codigoProfessor)
+
+}
