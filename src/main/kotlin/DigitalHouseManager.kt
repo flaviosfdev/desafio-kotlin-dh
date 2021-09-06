b0a5a3e66277afb6318009049165e54e9fe42159
Flávio Ferreira <flaviosf.dev@gmail.com>
2021-09-06 16:32:03 -0300

    Parte G

---
+++ src/main/kotlin/DigitalHouseManager.kt
@@ -0,0 +1,7 @@
+class DigitalHouseManager(
+    val alunos: MutableList<Aluno>,
+    val professores: MutableList<Professor>,
+    val cursos: MutableList<Curso>,
+    val matriculas: MutableList<Matricula>
+) {
+}
