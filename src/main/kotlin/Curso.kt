f7de971f12497b4f0c8afe49682b83c540dc9261
Flávio Ferreira <flaviosf.dev@gmail.com>
2021-09-06 16:57:50 -0300

    Parte H

--- src/main/kotlin/Curso.kt
+++ src/main/kotlin/Curso.kt
@@ -3,10 +3,33 @@ class Curso(
     val codigoCurso: Int,
     val professorTitular: ProfessorTitular,
     val professorAdjunto: ProfessorAdjunto,
-    val quantidadeMaximaAlunos: Int,
-    val alunosMatriculados: List<Aluno>
+    var quantidadeMaximaAlunos: Int,
+    val alunosMatriculados: MutableList<Aluno>
 ) {
+
+    init {
+        if (quantidadeMaximaAlunos <= 0) {
+            quantidadeMaximaAlunos = 10
+        }
+    }
 
     override fun equals(other: Any?): Boolean = (other is Curso && this.codigoCurso == other.codigoCurso)
 
+    fun adicionarUmAluno(umAluno: Aluno): Boolean {
+      if (alunosMatriculados.size < quantidadeMaximaAlunos) {
+          alunosMatriculados.add(umAluno)
+          return true
+      } else {
+          return false
+      }
+    }
+
+    fun excluirAluno(umAluno: Aluno): Unit{
+        if (alunosMatriculados.contains(umAluno)) {
+            alunosMatriculados.remove(umAluno)
+        } else {
+            println("Não foi possível realizar operação.")
+            println("${umAluno.nome} ${umAluno.sobrenome} não está matriculado no curso ${this.nome}")
+        }
+    }
 }
