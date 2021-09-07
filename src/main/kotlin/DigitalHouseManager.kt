92dcd8d6bd0f408da5a7cd80cfe6773f3574a9c7
Flávio Ferreira <flaviosf.dev@gmail.com>
2021-09-07 01:11:15 -0300

    Parte I - implementação (1) e (2)

--- src/main/kotlin/DigitalHouseManager.kt
+++ src/main/kotlin/DigitalHouseManager.kt
@@ -1,7 +1,66 @@
-class DigitalHouseManager(
-    val alunos: MutableList<Aluno>,
-    val professores: MutableList<Professor>,
-    val cursos: MutableList<Curso>,
-    val matriculas: MutableList<Matricula>
-) {
+class DigitalHouseManager {
+
+    val alunos: MutableList<Aluno> = mutableListOf()
+    val professores: MutableList<Professor> = mutableListOf()
+    val cursos: MutableList<Curso> = mutableListOf()
+    val matriculas: MutableList<Matricula> = mutableListOf()
+
+    fun registrarCurso(nome: String, codigoCurso: Int, quantidadeMaximaDeAlunos: Int) {
+
+        val cursoExistente: Curso? = cursos.find { it.codigoCurso == codigoCurso }
+
+        when (cursoExistente) {
+            null -> {
+                val novoCurso = Curso(nome, codigoCurso, quantidadeMaximaDeAlunos)
+                cursos.add(novoCurso)
+                println("Curso ${novoCurso.nome} (cód. ${novoCurso.codigoCurso}) cadastrado com sucesso!")
+            }
+            else -> {
+                println("Não foi possível cadastrar o curso: $nome - cód: $codigoCurso.")
+                println("Cód. ${cursoExistente.codigoCurso} já pertence ao curso: ${cursoExistente.nome}.")
+            }
+        }
+    }
+
+    fun excluirCurso(codigoCurso: Int) {
+
+        val curso : Curso?  = cursos.find { it.codigoCurso == codigoCurso }
+
+        when (curso) {
+            null -> {
+                println("Curso inexistente.")
+            }
+            else -> {
+                cursos.remove(curso)
+                println("Curso ${curso.nome} removido com sucesso.")
+            }
+        }
+
+    }
+
+    fun registrarProfessorAdjunto(nome: String, sobrenome: String, codigoProfessor: Int, quantidadeDeHoras: Int) {
+        TODO("registrar Professor Adjunto")
+    }
+
+    fun registrarProfessorTitular(nome: String, sobrenome: String, codigoProfessor: Int, especialidade: String) {
+        TODO("registrar Professor Titular")
+    }
+
+    fun excluirProfessor(codigoProfessor: Int) {
+        TODO("excluir Professor")
+    }
+
+    fun matricularAluno(nome: String, sobrenome: String, codigoAluno: Int) {
+        TODO("matricular aluno")
+    }
+
+    fun matricularAluno(codigoAluno: Int, codigoCurso: Int) {
+        TODO("matricular aluno em um curso")
+    }
+
+    fun alocarProfessores(codigoCurso: Int, codigoProfessorTitular: Int, codigoProfessorAdjunto: Int) {
+        TODO("alocar professores em um curso")
+    }
+
+
 }
